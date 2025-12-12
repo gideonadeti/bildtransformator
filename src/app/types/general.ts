eab13cc93ca6f0ac02cd3ea037c4e1c96e839598
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-12 22:26:12 +0000

    feat: add TransformedImage and Transformation interfaces for image processing
    - Introduced TransformedImage interface to represent images after transformation, including properties for size, secure URL, and transformation details.
    - Added Transformation interface to define the structure of image transformations, including options for resizing, cropping, rotating, and applying filters.
    - Included CropOptions and ResizeOptions interfaces to specify parameters for cropping and resizing images, enhancing the application's image handling capabilities.

--- src/app/types/general.ts
+++ src/app/types/general.ts
@@ -12,3 +12,38 @@ export interface Image {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface TransformedImage {
+  id: string;
+  originalImageId: string;
+  size: number;
+  secureUrl: string;
+  parentId: string | null;
+  createdAt: string;
+  updatedAt: string;
+
+  transformation: Transformation;
+}
+
+export interface Transformation {
+  order: Array<"resize" | "crop" | "rotate" | "grayscale" | "tint">;
+  rotate?: number;
+  grayscale?: boolean;
+  tint?: string;
+
+  crop?: CropOptions;
+  resize?: ResizeOptions;
+}
+
+export interface CropOptions {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+}
+
+export interface ResizeOptions {
+  width?: number;
+  height?: number;
+  fit?: "contain" | "cover" | "fill" | "inside" | "outside";
+}
