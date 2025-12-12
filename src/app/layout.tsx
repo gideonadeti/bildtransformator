c542071ac0a710406cfaa47337bae9924a178165
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-12 08:16:21 +0000

    feat: add Sonner for toast notifications
    - Integrated Sonner library for toast notifications in the application.
    - Updated layout to include a Toaster component for displaying notifications.
    - Added Sonner dependency to package.json and bun.lock for notification management.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 import AuthProvider from "./components/auth-provider";
 import QcProvider from "./components/qc-provider";
 import ThemeProvider from "./components/theme-provider";
@@ -37,6 +38,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
               disableTransitionOnChange
             >
               {children}
+              <Toaster richColors />
             </ThemeProvider>
           </AuthProvider>
         </QcProvider>
