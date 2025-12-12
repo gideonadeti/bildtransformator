271ef71a8652b30f2134d6f3d7435c4e6085b2a7
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-12 11:59:36 +0000

    feat: enhance authentication types and form value inference
    - Added new SignUpInResponse interface for handling user authentication responses.
    - Introduced type inference for form values using zod schemas for sign up, sign in, forgot password, and reset password functionalities.
    - Improved type definitions for better integration with form validation and user management.

--- src/app/types/auth.ts
+++ src/app/types/auth.ts
@@ -1,5 +1,24 @@
+import type { z } from "zod";
+
+import type {
+  forgotPasswordFormSchema,
+  resetPasswordFormSchema,
+  signInFormSchema,
+  signUpFormSchema,
+} from "../libs/form-schemas/auth-form-schemas";
+
+export interface SignUpInResponse {
+  accessToken: string;
+  user: User;
+}
+
 export interface User {
   id: string;
   name: string;
   email: string;
 }
+
+export type SignUpFormValues = z.infer<typeof signUpFormSchema>;
+export type SignInFormValues = z.infer<typeof signInFormSchema>;
+export type ForgotPasswordFormValues = z.infer<typeof forgotPasswordFormSchema>;
+export type ResetPasswordFormValues = z.infer<typeof resetPasswordFormSchema>;
