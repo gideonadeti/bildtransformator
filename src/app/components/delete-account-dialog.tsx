7604ce29aabffdc1fd850067bc5c1dcffe2fed9f
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-12 12:02:47 +0000

    feat: add DeleteAccountDialog component for account deletion confirmation
    - Introduced DeleteAccountDialog component to confirm account deletion with user prompts.
    - Integrated useAuth hook for handling account deletion logic.
    - Enhanced user experience by providing clear actions and feedback during the deletion process.

---
+++ src/app/components/delete-account-dialog.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import useAuth from "@/app/hooks/use-auth";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import CustomDialogFooter from "./custom-dialog-footer";
+
+interface DeleteAccountDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+const DeleteAccountDialog = ({
+  open,
+  onOpenChange,
+}: DeleteAccountDialogProps) => {
+  const { deleteAccountMutation } = useAuth();
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Delete Account</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete your account? This action cannot be
+            undone. This will permanently delete your account and your data.
+          </DialogDescription>
+        </DialogHeader>
+        <CustomDialogFooter
+          normalText="Delete Account"
+          pendingText="Deleting Account..."
+          isPending={deleteAccountMutation.isPending}
+          handleCancel={() => onOpenChange(false)}
+          handleSubmit={() => deleteAccountMutation.mutate({ onOpenChange })}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DeleteAccountDialog;
