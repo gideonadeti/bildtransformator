c6a85cc8ec75691484f65177714a474b2d68e3fb
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-26 13:12:17 +0000

    feat: enhance useTransformedImage hook with mutation for image transformation
    - Added a new mutation `transformTransformedImageMutation` to handle image transformation requests, including success and error notifications.
    - Integrated the useRouter hook for navigation upon successful transformation, improving user experience.
    - Updated the hook to return the new mutation alongside the existing query, enhancing its functionality.

--- src/app/hooks/use-transformed-image.ts
+++ src/app/hooks/use-transformed-image.ts
@@ -1,15 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "sonner";
 
-import type { TransformedImage } from "../types/general";
-import { fetchTransformedImage } from "../utils/general-query-functions";
+import type {
+  TransformedImage,
+  TransformImageFormValues,
+} from "../types/general";
+import {
+  fetchTransformedImage,
+  transformTransformedImage,
+} from "../utils/general-query-functions";
 import useAccessToken from "./use-access-token";
 
 const useTransformedImage = (id: string) => {
   const { accessToken } = useAccessToken();
-
+  const router = useRouter();
   const transformedImageQuery = useQuery<
     TransformedImage,
     AxiosError<{ message: string }>
@@ -32,8 +39,49 @@ const useTransformedImage = (id: string) => {
     transformedImageQuery.isError,
   ]);
 
+  const transformTransformedImageMutation = useMutation<
+    { jobId: string } | TransformedImage,
+    AxiosError<{ message: string }>,
+    {
+      id: string;
+      formValues: TransformImageFormValues;
+      onOpenChange: (open: boolean) => void;
+    }
+  >({
+    mutationFn: async ({ id, formValues }) => {
+      return transformTransformedImage(id, formValues);
+    },
+    onError: (error) => {
+      const message =
+        error.response?.data?.message ||
+        "Failed to transform transformed image";
+
+      toast.error(message, { id: "transform-transformed-image-error" });
+    },
+    onSuccess: (data, { id, onOpenChange }) => {
+      onOpenChange(false);
+
+      if ("id" in data && "originalImageId" in data) {
+        toast.success("Transformed image transformation already exists", {
+          id: "transform-transformed-image-already-exists",
+          action: {
+            label: "View",
+            onClick: () => {
+              router.push(`/transformed-images/${id}#${data.id}`);
+            },
+          },
+        });
+      } else {
+        toast.success("Transformed image transformation started", {
+          id: "transform-transformed-image-started",
+        });
+      }
+    },
+  });
+
   return {
     transformedImageQuery,
+    transformTransformedImageMutation,
   };
 };
 
